ab917c1434597b871e359783cd893b27e309edd2
alphasun07 <Dothingocanh0712>
2022-03-07 01:15:19 +0700

    header of detail page

--- js/script.js
+++ js/script.js
@@ -5,4 +5,15 @@ $('.autoplay').slick({
     autoplay: false,
     autoplaySpeed: 2000,
   });
-      
+
+window.onscroll = function(){
+  scroll();
+};
+function scroll(){
+    if(document.body.scrollTop > 350 || document.documentElement.scrollTop > 350){
+        document.getElementById("product-scroll-section").style.top = "0";
+    }
+    else{
+        document.getElementById("product-scroll-section").style.top = "-300px";
+    }
+}
